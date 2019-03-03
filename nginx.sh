4c5ed451ff9157da175ce0065121212695dc923b
Rob Kerry <rob@landdigital.co.uk>
2019-03-03 14:01:02 +0000

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -92,7 +92,7 @@ sudo mv -f sshd_config /etc/ssh/sshd_config
 
 ### Configure NGINX ###
 
-sudo apt install -y php7.2-fpm php7.2-cli php7.2-sqlite3 php7.2-mysql php7.2-gd php7.2-curl php7.2-memcached php7.2-imap php7.2-mbstring php7.2-xml php7.2-zip php7.2-bcmath php7.2-soap php7.2-intl php7.2-readline php7.2-dev
+sudo apt install -y nginx php7.2-fpm php7.2-cli php7.2-sqlite3 php7.2-mysql php7.2-gd php7.2-curl php7.2-memcached php7.2-imap php7.2-mbstring php7.2-xml php7.2-zip php7.2-bcmath php7.2-soap php7.2-intl php7.2-readline php7.2-dev
 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
